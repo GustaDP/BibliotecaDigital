92028a8d9045595c94092101077fa38559d9e7ff
unknown <babystarkwarrior@gmail.com>
2025-10-24 19:20:42 -0300

    ~CSS alterado :) �

--- contas/forms.py
+++ contas/forms.py
@@ -3,9 +3,57 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Usuario
 
 class CadastroForm(UserCreationForm):
+    nome = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-field',
+            'placeholder': 'Digite seu nome completo'
+        })
+    )
+    ra = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-field',
+            'placeholder': 'Digite seu RA'
+        })
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-field',
+            'placeholder': 'Digite seu email'
+        })
+    )
+    ano_escolar = forms.ChoiceField(
+        choices=Usuario.ANOS,
+        widget=forms.Select(attrs={
+            'class': 'form-field'
+        })
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-field',
+            'placeholder': 'Digite sua senha'
+        })
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-field',
+            'placeholder': 'Confirme sua senha'
+        })
+    )
+
     class Meta:
         model = Usuario
         fields = ['nome', 'ra', 'email', 'ano_escolar', 'password1', 'password2']
 
 class LoginForm(AuthenticationForm):
-    username = forms.EmailField(label='Email')
+    username = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-field',
+            'placeholder': 'Digite seu email'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-field',
+            'placeholder': 'Digite sua senha'
+        })
+    )
